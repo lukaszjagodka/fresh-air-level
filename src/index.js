9ed4f26dec3656279429420f2364199a79d004ab
encorey <lukasz.jagodka@protonmail.com>
2019-12-30 19:55:28 +0100

    Code improvement

--- src/index.js
+++ src/index.js
@@ -7,16 +7,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers/index';
 
-// function saveToLocalStorage(state) {
-//   try {
-//     const serializedState = JSON.stringify(state);
-//     localStorage.setItem('state', serializedState);
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
+
 const store = createStore(rootReducer, applyMiddleware(thunk));
-// store.subscribe(()=> saveToLocalStorage(store.getState()));
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
